43fe994d237f5f90328a1ff8f6932f94398d037c
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2023-05-18 12:13:23 -0400

    server/ registrando o jwt

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import fastifyJwt from '@fastify/jwt'
 
 const app = fastify()
 
@@ -8,11 +11,17 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(fastifyJwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3001,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('listening on port 3001')
