e6d391e199d64efced73d2d7bb65fc219127e7fd
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2023-05-18 12:15:33 -0400

    web/ componente de Copyright (c)

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,17 @@
+function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da Rocketseat{' '}
+      <a
+        href="https://www.rocketseat.com.br/"
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
+
+export default Copyright
