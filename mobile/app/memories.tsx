9ba5fbf13776a27892a0856d79b980df9a046cac
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2023-05-18 12:09:11 -0400

    pagina de momorias

---
+++ mobile/app/memories.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+function Memories() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Memorias</Text>
+    </View>
+  )
+}
+
+export default Memories
